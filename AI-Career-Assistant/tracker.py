2a3afb92e29faa98f470f5a56adeac153f9c44d1
Aakash Kathir <119592109+15Aakash@users.noreply.github.com>
2026-05-24 18:44:36 -0400

    tracker.py

--- AI-Career-Assistant/tracker.py
+++ AI-Career-Assistant/tracker.py
@@ -1,43 +1,7 @@
 import pandas as pd
-from datetime import date
 import os
 
 FILE_NAME = "applications.csv"
-
-
-def save_application(company, role, job_link, status, notes):
-
-    new_data = pd.DataFrame([{
-        "date": date.today(),
-        "company": company,
-        "role": role,
-        "job_link": job_link,
-        "status": status,
-        "notes": notes
-        "interview_date": ""
-    }])
-
-    if os.path.exists(FILE_NAME):
-
-        data = pd.read_csv(FILE_NAME)
-
-        duplicate = data[
-            (data["company"].str.lower() == company.lower()) &
-            (data["role"].str.lower() == role.lower()) &
-            (data["job_link"].str.lower() == job_link.lower())
-        ]
-
-        if not duplicate.empty:
-            return False
-
-        data = pd.concat([data, new_data], ignore_index=True)
-
-    else:
-        data = new_data
-
-    data.to_csv(FILE_NAME, index=False)
-
-    return True
 
 
 def load_applications():
@@ -45,18 +9,66 @@ def load_applications():
     if os.path.exists(FILE_NAME):
         return pd.read_csv(FILE_NAME)
 
-    return pd.DataFrame()
+    columns = [
+        "date",
+        "company",
+        "role",
+        "job_link",
+        "status",
+        "notes",
+        "interview_date"
+    ]
+
+    return pd.DataFrame(columns=columns)
+
+
+def save_application(
+    company,
+    role,
+    job_link,
+    status,
+    notes
+):
+
+    data = load_applications()
+
+    duplicate = data[
+        (data["company"] == company)
+        &
+        (data["role"] == role)
+    ]
+
+    if not duplicate.empty:
+        return False
+
+    new_row = {
+        "date": pd.Timestamp.now().strftime("%Y-%m-%d"),
+        "company": company,
+        "role": role,
+        "job_link": job_link,
+        "status": status,
+        "notes": notes,
+        "interview_date": ""
+    }
+
+    data = pd.concat(
+        [data, pd.DataFrame([new_row])],
+        ignore_index=True
+    )
+
+    data.to_csv(FILE_NAME, index=False)
+
+    return True
 
 
 def delete_application(index):
 
-    if os.path.exists(FILE_NAME):
+    data = load_applications()
 
-        data = pd.read_csv(FILE_NAME)
+    data = data.drop(index)
 
-        data = data.drop(index)
+    data.to_csv(FILE_NAME, index=False)
 
-        data.to_csv(FILE_NAME, index=False)
 
 def update_application_status(index, new_status):
 
@@ -65,7 +77,8 @@ def update_application_status(index, new_status):
     data.loc[index, "status"] = new_status
 
     data.to_csv(FILE_NAME, index=False)
-    
+
+
 def update_interview_date(index, interview_date):
 
     data = load_applications()
